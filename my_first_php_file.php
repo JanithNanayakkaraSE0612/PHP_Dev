e49c019f0cb200e98e080addcdf23982d1e8c199
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-08-17 00:50:03 +0530

    added

---
+++ my_first_php_file.php
@@ -0,0 +1,10 @@
+<?php echo "This is my first PHP file.";
+echo "<br>";
+echo "This is my first PHP file.";
+echo "<br>";
+echo "First Name" . "Secnond Name";
+echo "<br>";
+echo 6;
+echo "<br>";
+echo 6+4;
+?>
