79833b7af889b0a4710dbd7cb6565d1f8f9a18f7
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-03 21:54:05 +0530

    Complete Code

--- String.php
+++ String.php
@@ -3,4 +3,7 @@
     $last_name = "Nanayakkara";
 
     echo $first_name . " " .$last_name;
+    echo "<br>";
+    echo "Ful name is :" .$first_name . " " . $last_name;
+    echo"Full name is : {$first_name} {$last_name}";
 ?>
