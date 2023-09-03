429af4cefc66999c17e287dc3899aaf9879a2e23
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-03 19:07:16 +0530

    Finish

---
+++ variable_php.php
@@ -0,0 +1,12 @@
+<?php
+    //varibales
+
+    $varible_name = "value";
+
+    $first_name = "Janith";
+    echo $first_name;
+    $age = 30;
+    echo "<br>";
+    echo $age;
+    //php varible case sensitive
+?>
