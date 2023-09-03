67610effab05fae2e2b39a795804dec0b4f2bd48
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-03 22:38:58 +0530

    normally function

--- String_Function.php
+++ String_Function.php
@@ -12,3 +12,12 @@ echo $sentence;
 All Simple : <?php echo strtolower($sentence);?>
 <br>
 All Capital : <?php echo strtoupper($sentence);?>
+<br>
+First Letter capital : <?php echo ucfirst($sentence); ?>
+<br>
+First letter capital in all words : <?php echo ucwords($sentence); ?>
+<br>
+Number of Characters : <?php echo strlen($sentence);?>
+<br>
+Number
+
